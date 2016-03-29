da782df1f542bdd1d681957be90e4339146d133d
Ray De Castro <captspock@live.com>
2016-03-29 16:13:19 -0700

    cleanup. removed unused elements. changed some ids to classNames.

--- source/js/components/app.react.js
+++ source/js/components/app.react.js
@@ -2,7 +2,7 @@ var App = React.createClass({
     
     render: function() {
         return (
-            <div id="application">
+            <div className="application">
                 <Header/>
                 <WeatherPanel date="Tuesday 3/29" sunrise="08:00" sunset="17:00" moonrise="09:00" moonset="18:00" />
             </div>
