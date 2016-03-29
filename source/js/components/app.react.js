0e3f164fedf05841d130a5a1edf44f5dc0621b71
Ray De Castro <captspock@live.com>
2016-03-29 16:08:21 -0700

    feature. made hardcoded values into properties.

--- source/js/components/app.react.js
+++ source/js/components/app.react.js
@@ -4,7 +4,7 @@ var App = React.createClass({
         return (
             <div id="application">
                 <Header/>
-                <WeatherPanel/>
+                <WeatherPanel date="Tuesday 3/29" sunrise="08:00" sunset="17:00" moonrise="09:00" moonset="18:00" />
             </div>
         );
     }
--- source/js/components/weatherPanel.react.js
+++ source/js/components/weatherPanel.react.js
@@ -3,37 +3,37 @@ var WeatherPanel = React.createClass({
     render: function() {
         return (
             <div className="weather-panel">
-            <div className="panel panel-primary">
-                <div className="panel-heading">Sun</div>
-                <div className="panel-body">{ Date() }</div>
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <th>Sunrise</th>
-                            <td>00:00</td>
-                        </tr>
-                        <tr>
-                            <th>Sunset</th>
-                            <td>00:00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div className="panel panel-primary">
-                <div className="panel-heading">Moon</div>
-                <table className="table">
-                    <tbody>
-                        <tr>
-                            <th>Moonrise</th>
-                            <td>00:00</td>
-                        </tr>
-                        <tr>
-                            <th>Moonset</th>
-                            <td>00:00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                <h2 className="date">{this.props.date}</h2>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">Sun</div>
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th>Sunrise</th>
+                                <td>{this.props.sunrise}</td>
+                            </tr>
+                            <tr>
+                                <th>Sunset</th>
+                                <td>{this.props.sunset}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">Moon</div>
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th>Moonrise</th>
+                                <td>{this.props.moonrise}</td>
+                            </tr>
+                            <tr>
+                                <th>Moonset</th>
+                                <td>{this.props.moonset}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
